4a639d038d4277e7f6c89f4ccf51e54745bb3292
SURYA PRAKASH <surya@hackerrank.com>
2016-08-28 19:10:21 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -3,153 +3,173 @@ var clientID;
 var clientSecret;
 var markers = [];
 
-var initialLocations = [
-  {name: 'Oasis Center', lat: 12.9375433,long: 77.6280165},
-  {name: 'Barleyz', lat: 12.9376218,long: 77.6269792},
-  {name: 'HDFC Bank ATM', lat: 12.9386243,long: 77.6304922},
-  {name: 'Food Affairs', lat: 12.9383949,long: 77.6304051},
-  {name: 'Bak Bak Bar', lat: 12.9380433,long: 77.6277269},
-];
+var initialLocations = [{
+    name: 'Oasis Center',
+    lat: 12.9375433,
+    long: 77.6280165
+}, {
+    name: 'Barleyz',
+    lat: 12.9376218,
+    long: 77.6269792
+}, {
+    name: 'HDFC Bank ATM',
+    lat: 12.9386243,
+    long: 77.6304922
+}, {
+    name: 'Food Affairs',
+    lat: 12.9383949,
+    long: 77.6304051
+}, {
+    name: 'Bak Bak Bar',
+    lat: 12.9380433,
+    long: 77.6277269
+}, ];
 
 function formatPhone(phonenum) {
     var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
     if (regexObj.test(phonenum)) {
         var parts = phonenum.match(regexObj);
         var phone = "";
-        if (parts[1]) { phone += "+1 (" + parts[1] + ") "; }
+        if (parts[1]) {
+            phone += "+1 (" + parts[1] + ") ";
+        }
         phone += parts[2] + "-" + parts[3];
         return phone;
-    }
-    else {
+    } else {
         //invalid phone number
         return phonenum;
     }
 }
 
 var Location = function(data) {
-	var self = this;
-	this.name = data.name;
-	this.lat = data.lat;
-	this.long = data.long;
-	this.URL = "";
-	this.street = "";
-	this.city = "";
-	this.phone = "";
+    var self = this;
+    this.name = data.name;
+    this.lat = data.lat;
+    this.long = data.long;
+    this.URL = "";
+    this.street = "";
+    this.city = "";
+    this.phone = "";
 
-	this.visible = ko.observable(true);
+    this.visible = ko.observable(true);
 
-	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+
-  this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret +
-  '&v=20140806' + '&query=' + this.name;
+    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' +
+        this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret +
+        '&v=20140806' + '&query=' + this.name;
 
-	$.getJSON(foursquareURL).done(function(data) {
-		var results = data.response.venues[0];
-		self.URL = results.url;
-		if (typeof self.URL === 'undefined'){
-			self.URL = "";
-		}
-		self.street = results.location.formattedAddress[0];
-     	self.city = results.location.formattedAddress[1];
-      	self.phone = results.contact.phone;
-      	if (typeof self.phone === 'undefined'){
-			self.phone = "";
-		} else {
-			self.phone = formatPhone(self.phone);
-		}
-	}).fail(function() {
-		alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
-	});
+    $.getJSON(foursquareURL).done(function(data) {
+        var results = data.response.venues[0];
+        self.URL = results.url;
+        if (typeof self.URL === 'undefined') {
+            self.URL = "";
+        }
+        self.street = results.location.formattedAddress[0];
+        self.city = results.location.formattedAddress[1];
+        self.phone = results.contact.phone;
+        if (typeof self.phone === 'undefined') {
+            self.phone = "";
+        } else {
+            self.phone = formatPhone(self.phone);
+        }
+    }).fail(function() {
+        alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
+    });
 
-	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+    this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
         '<div class="content">' + self.street + "</div>" +
         '<div class="content">' + self.city + "</div>" +
         '<div class="content">' + self.phone + "</div></div>";
 
-	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+    this.infoWindow = new google.maps.InfoWindow({
+        content: self.contentString
+    });
 
-	this.marker = new google.maps.Marker({
-			position: new google.maps.LatLng(data.lat, data.long),
-			map: map,
-			title: data.name
-	});
+    this.marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.long),
+        map: map,
+        title: data.name
+    });
 
-	this.showMarker = ko.computed(function() {
-		if(this.visible() === true) {
-			this.marker.setMap(map);
-		} else {
-			this.marker.setMap(null);
-		}
-		return true;
-	}, this);
+    this.showMarker = ko.computed(function() {
+        if (this.visible() === true) {
+            this.marker.setMap(map);
+        } else {
+            this.marker.setMap(null);
+        }
+        return true;
+    }, this);
 
-	this.marker.addListener('click', function(){
-		self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
-        '<div class="content">' + self.street + "</div>" +
-        '<div class="content">' + self.city + "</div>" +
-        '<div class="content"><a href="tel:' + self.phone +'">' + self.phone +"</a></div></div>";
+    this.marker.addListener('click', function() {
+        self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+            '<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
+            '<div class="content">' + self.street + "</div>" +
+            '<div class="content">' + self.city + "</div>" +
+            '<div class="content"><a href="tel:' + self.phone + '">' + self.phone + "</a></div></div>";
 
         self.infoWindow.setContent(self.contentString);
 
-		self.infoWindow.open(map, this);
+        self.infoWindow.open(map, this);
 
-		self.marker.setAnimation(google.maps.Animation.BOUNCE);
-      	setTimeout(function() {
-      		self.marker.setAnimation(null);
-     	}, 2100);
-	});
+        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            self.marker.setAnimation(null);
+        }, 2100);
+    });
 
-	this.bounce = function(place) {
-		google.maps.event.trigger(self.marker, 'click');
-	};
+    this.bounce = function(place) {
+        google.maps.event.trigger(self.marker, 'click');
+    };
 };
 
 function AppViewModel() {
-	var self = this;
+    var self = this;
 
-	this.searchTerm = ko.observable("");
+    this.searchTerm = ko.observable("");
 
-	this.locationList = ko.observableArray([]);
+    this.locationList = ko.observableArray([]);
 
-	map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 12,
-			center: {lat: 12.938533, long: 77.630817}
-	});
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: {
+            lat: 12.938533,
+            long: 77.630817
+        }
+    });
 
-	// Foursquare API settings
-	clientID = "RXTKNR13PAJ1HTJ5C0S5G0FR4OANS3A3XFWNLIGDGNN3Q0VX";
-	clientSecret = "OZSMXEMARDPACMGMTUEXHVGLG1QDD123JB4TLHD0SYYCKFP0";
+    // Foursquare API settings
+    clientID = "RXTKNR13PAJ1HTJ5C0S5G0FR4OANS3A3XFWNLIGDGNN3Q0VX";
+    clientSecret = "OZSMXEMARDPACMGMTUEXHVGLG1QDD123JB4TLHD0SYYCKFP0";
 
-	initialLocations.forEach(function(locationItem){
-		self.locationList.push( new Location(locationItem));
-	});
+    initialLocations.forEach(function(locationItem) {
+        self.locationList.push(new Location(locationItem));
+    });
 
-	this.filteredList = ko.computed( function() {
-		var filter = self.searchTerm().toLowerCase();
-		if (!filter) {
-			self.locationList().forEach(function(locationItem){
-				locationItem.visible(true);
-			});
-			return self.locationList();
-		} else {
-			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
-				var string = locationItem.name.toLowerCase();
-				var result = (string.search(filter) >= 0);
-				locationItem.visible(result);
-				return result;
-			});
-		}
-	}, self);
+    this.filteredList = ko.computed(function() {
+        var filter = self.searchTerm().toLowerCase();
+        if (!filter) {
+            self.locationList().forEach(function(locationItem) {
+                locationItem.visible(true);
+            });
+            return self.locationList();
+        } else {
+            return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+                var string = locationItem.name.toLowerCase();
+                var result = (string.search(filter) >= 0);
+                locationItem.visible(result);
+                return result;
+            });
+        }
+    }, self);
 
-	this.mapElem = document.getElementById('map');
-	this.mapElem.style.height = window.innerHeight - 50;
+    this.mapElem = document.getElementById('map');
+    this.mapElem.style.height = window.innerHeight - 50;
 }
 
 function startApp() {
-	ko.applyBindings(new AppViewModel());
+    ko.applyBindings(new AppViewModel());
 }
 
 function errorHandling() {
-	alert("Google Maps has failed to load. Please check your internet connection and try again.");
+    alert("Google Maps has failed to load. Please check your internet connection and try again.");
 }
