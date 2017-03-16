3384eaebd9ee379e875c0ec4c3896a3c834c0e4a
SURYA PRAKASH <surya2902@gmail.com>
2017-03-16 10:16:43 +0530

    change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','data.json');
 ourRequest.onload() = function(){
-  var ourData = ourRequest.responseText;
+  var ourData = JSON.parse(ourRequest.responseText);
   console.log(ourData[0]);
 };
 ourRequest.send();
